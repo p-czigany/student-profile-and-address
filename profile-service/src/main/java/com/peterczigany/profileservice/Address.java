5778272e54ffcecddcc6552c135d51ba79192b9f
Peter Czigany <p.czigany@gmail.com>
2023-10-09 01:49:18 +0200

    fix: add empty constructor to the model

--- profile-service/src/main/java/com/peterczigany/profileservice/Address.java
+++ profile-service/src/main/java/com/peterczigany/profileservice/Address.java
@@ -3,13 +3,16 @@ package com.peterczigany.profileservice;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class Address {
 
   private UUID id;
-  private String schoolAddress;
+//  private String id;
+  private String address;
 
 }
