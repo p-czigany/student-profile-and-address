e255e29ffe40a4c64c7376e344c4f3d4587119ce
Peter Czigany <p.czigany@gmail.com>
2023-10-08 11:52:16 +0200

    chore: clean up contract and http

--- src/main/java/com/peterczigany/studentservice/StudentHttpConfiguration.java
+++ src/main/java/com/peterczigany/studentservice/StudentHttpConfiguration.java
@@ -5,6 +5,9 @@ import static org.springframework.web.servlet.function.RouterFunctions.route;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
@@ -15,6 +18,9 @@ public class StudentHttpConfiguration {
   RouterFunction<ServerResponse> routes(@Autowired StudentRepository repository) {
     return route()
         .GET("/students", request -> ServerResponse.ok().body(repository.findAll()))
+//        .POST("/students", RequestPredicates.accept(MediaType.APPLICATION_JSON),
+//            request -> ServerResponse.status(HttpStatus.CREATED)
+//                .body(repository.save(request.body(Student.class))))
         .build();
   }
 }
--- src/test/resources/contracts/shouldReturnAllStudents.groovy
+++ src/test/resources/contracts/shouldReturnAllStudents.groovy
@@ -2,21 +2,23 @@ package contracts
 
 import org.springframework.cloud.contract.spec.Contract
 import org.springframework.cloud.contract.spec.internal.HttpMethods
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 
 Contract.make {
-    description("/students")
+    description("Should return all students")
     request {
         url("/students")
         method(HttpMethods.GET)
     }
     response {
-
-        status(200)
+        status(HttpStatus.OK.value())
         body([
                 [id: UUID.fromString("123e4567-e89b-12d3-a456-426614174000"), name: "Joe", email: "joe@school.com"],
                 [id: UUID.fromString("123e4567-e89b-12d3-a456-426614174001"), name: "Ashley", email: "ashley@school.com"]
         ])
-        headers { contentType(MediaType.APPLICATION_JSON_VALUE) }
+        headers {
+            contentType(MediaType.APPLICATION_JSON_VALUE)
+        }
     }
 }
