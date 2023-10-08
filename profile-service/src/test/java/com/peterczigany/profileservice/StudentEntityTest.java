a40979568316f05263c0f6e95d4195b085b4a4a6
Peter Czigany <p.czigany@gmail.com>
2023-10-09 00:40:05 +0200

    chore: consolidate entity tests

--- profile-service/src/test/java/com/peterczigany/profileservice/StudentEntityTest.java
+++ profile-service/src/test/java/com/peterczigany/profileservice/StudentEntityTest.java
@@ -1,33 +1,83 @@
 package com.peterczigany.profileservice;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.r2dbc.core.DatabaseClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Hooks;
 import reactor.test.StepVerifier;
+
+import java.util.Arrays;
+import java.util.List;
 
 @DataR2dbcTest
 class StudentEntityTest {
 
-  @Autowired
-  private StudentRepository repository;
+    @Autowired
+    private StudentRepository repository;
 
-  @Test
-  void persist() throws Exception {
+    @Autowired
+    private DatabaseClient dbClient;
 
-    Flux<Student> students = Flux.just(new Student(null, "Joe", "joe@school.com"),
-        new Student(null, "Ashley", "ashley@school.com"));
-    Flux<Student> retrievedStudents = this.repository.saveAll(students);
+    @BeforeEach
+    public void setup() {
 
-    StepVerifier
-        .create(retrievedStudents)
-        .expectNextCount(1)
-        .expectNextMatches(student -> student.getName().equals("Joe"))
-//        .expectNextMatches(student -> student.getName().equals("Ashley"))
-        .verifyComplete();
-//    Assertions.assertThat(retrievedStudent.getName()).isEqualTo("Joe");
-//    Assertions.assertThat(retrievedStudent.getId()).isNotNull();
-  }
+        Hooks.onOperatorDebug();
+
+        List<String> statements = Arrays.asList(
+                "DROP TABLE IF EXISTS student;",
+                "CREATE table student (id UUID DEFAULT UUID(), name VARCHAR2, email VARCHAR2);");
+
+        statements.forEach(it -> dbClient.sql(it)
+                .fetch()
+                .rowsUpdated()
+                .as(StepVerifier::create)
+                .expectNextCount(1)
+                .verifyComplete());
+    }
+
+    @Test
+    void persist() throws Exception {
+
+        Flux<Student> students = Flux.just(new Student(null, "Joe", "joe@school.com"),
+                new Student(null, "Ashley", "ashley@school.com"));
+        Flux<Student> retrievedStudents = repository.saveAll(students);
+
+        StepVerifier
+                .create(retrievedStudents)
+                .expectNextCount(1)
+                .expectNextMatches(student -> student.getName().equals("Ashley"))
+                .verifyComplete();
+    }
+
+    @Test
+    void whenDeleteAll_then0IsExpected() {
+        repository.deleteAll()
+                .as(StepVerifier::create)
+                .expectNextCount(0)
+                .verifyComplete();
+    }
+
+    @Test
+    void whenInsert2_then2AreExpected() {
+        insertStudents();
+        repository.findAll()
+                .as(StepVerifier::create)
+                .expectNextCount(2)
+                .verifyComplete();
+    }
+
+    private void insertStudents() {
+        List<Student> students = Arrays.asList(
+                new Student(null, "Joe",
+                        "joe@school.com"),
+                new Student(null, "Ashley",
+                        "ashley@school.com")
+        );
+
+        repository.saveAll(students).subscribe();
+    }
 
 }
