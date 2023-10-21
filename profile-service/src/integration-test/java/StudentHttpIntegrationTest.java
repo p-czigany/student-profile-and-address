be657bc0a92e7cad6a3f0f7569d9da0ca84b3743
Peter Czigany <p.czigany@gmail.com>
2023-10-21 16:14:42 +0200

    refactor: make integration tests independent

--- profile-service/src/integration-test/java/StudentHttpIntegrationTest.java
+++ profile-service/src/integration-test/java/StudentHttpIntegrationTest.java
@@ -1,10 +1,7 @@
 import com.peterczigany.profileservice.ProfileServiceApplication;
 import com.peterczigany.profileservice.model.Student;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +13,6 @@ import reactor.core.publisher.Mono;
 
 @SpringBootTest(classes = ProfileServiceApplication.class)
 @AutoConfigureWebTestClient
-@TestMethodOrder(OrderAnnotation.class)
 class StudentHttpIntegrationTest {
   private final WebTestClient testClient;
 
@@ -26,8 +22,7 @@ class StudentHttpIntegrationTest {
   }
 
   @Test
-  @Order(1)
-  void givenNoStudents_whenGetStudents_thenReturnWithEmptyList() {
+  void testGet() {
 
     testClient
         .get()
@@ -37,52 +32,21 @@ class StudentHttpIntegrationTest {
         .isOk()
         .expectHeader()
         .contentTypeCompatibleWith(MediaType.APPLICATION_JSON)
-        .expectBodyList(Student.class)
-        .hasSize(0);
+        .expectBodyList(Student.class);
   }
 
   @ParameterizedTest
   @CsvSource({
-    "Edward Sapir,sapir@school.com",
-    "Annabelle,annabelle@school.com",
-    "Benjamin Lee Whorf,whorf@school.com",
-    "Anatoly Khazanov,khazanov@school.com",
-    "Claude Lévi-Strauss,levi-strauss@school.com"
+    "Edward Sapir,sapir@school.com,200"
   })
-  @Order(2)
-  void shouldReturnSameNameAndEmail_whenStudentIsPosted(String name, String email) {
+  void testPost(String name, String email, int responseStatus) {
     Student student = new Student(null, name, email);
-    Student response =
-        testClient
-            .post()
-            .uri("/students")
-            .body(Mono.just(student), Student.class)
-            .exchange()
-            .expectStatus()
-            .isOk()
-            .expectHeader()
-            .contentTypeCompatibleWith(MediaType.APPLICATION_JSON)
-            .expectBody(Student.class)
-            .returnResult()
-            .getResponseBody();
-    assert response != null;
-    Assertions.assertThat(response.getName()).isEqualTo(student.getName());
-    Assertions.assertThat(response.getEmail()).isEqualTo(student.getEmail());
-  }
-
-  @Test
-  @Order(3)
-  void givenStudents_whenGetStudents_thenReturnStudentList() {
-
     testClient
-        .get()
+        .post()
         .uri("/students")
+        .body(Mono.just(student), Student.class)
         .exchange()
         .expectStatus()
-        .isOk()
-        .expectHeader()
-        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON)
-        .expectBodyList(Student.class)
-        .hasSize(5);
+        .isEqualTo(responseStatus);
   }
 }
