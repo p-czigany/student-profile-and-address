495bc7f987d5fc19254e0d5e8b232ea66f509585
Peter Czigany <p.czigany@gmail.com>
2023-10-26 12:37:19 +0200

    fix: patching with ungiven fields will not overwrite existing data with null

--- profile-service/src/main/java/com/peterczigany/profileservice/controller/StudentController.java
+++ profile-service/src/main/java/com/peterczigany/profileservice/controller/StudentController.java
@@ -3,26 +3,17 @@ package com.peterczigany.profileservice.controller;
 import com.peterczigany.profileservice.dto.StudentDTO;
 import com.peterczigany.profileservice.model.Student;
 import com.peterczigany.profileservice.repository.StudentRepository;
-import com.peterczigany.profileservice.validator.StudentPostValidator;
 import java.util.UUID;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
-import org.springframework.validation.BeanPropertyBindingResult;
-import org.springframework.validation.Errors;
-import org.springframework.validation.Validator;
-import org.springframework.web.reactive.function.server.ServerRequest;
-import org.springframework.web.reactive.function.server.ServerResponse;
-import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
 public class StudentController {
 
-  private static final ModelMapper mapper = new ModelMapper();
+  @Autowired ModelMapper mapper;
 
   @Autowired private StudentRepository repository;
 
