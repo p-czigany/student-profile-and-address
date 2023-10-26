d4aceb7f2667502827792975a8da89a4b200078f
Peter Czigany <p.czigany@gmail.com>
2023-10-26 15:00:10 +0200

    fix: patching will response with HTTP 422 if id is not found

--- profile-service/src/main/java/com/peterczigany/profileservice/configuration/StudentPatchValidationHandler.java
+++ profile-service/src/main/java/com/peterczigany/profileservice/configuration/StudentPatchValidationHandler.java
@@ -2,13 +2,14 @@ package com.peterczigany.profileservice.configuration;
 
 import com.peterczigany.profileservice.controller.StudentController;
 import com.peterczigany.profileservice.dto.StudentDTO;
-import com.peterczigany.profileservice.model.Student;
 import com.peterczigany.profileservice.validator.StudentPatchValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
 
 @Component
@@ -23,9 +24,20 @@ public class StudentPatchValidationHandler
 
   @Override
   protected Mono<ServerResponse> processBody(StudentDTO validBody, ServerRequest originalRequest) {
-    return ServerResponse.ok()
-        .contentType(MediaType.APPLICATION_JSON)
-        .body(
-            controller.updateStudent(originalRequest.pathVariable("id"), validBody), Student.class);
+    return controller
+        .updateStudent(originalRequest.pathVariable("id"), validBody)
+        .switchIfEmpty(
+            Mono.error(
+                new ResponseStatusException(
+                    HttpStatus.UNPROCESSABLE_ENTITY, "No student is found with the given id.")))
+        .flatMap(
+            student ->
+                ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).bodyValue(student))
+        .onErrorResume(
+            ResponseStatusException.class,
+            ex ->
+                ServerResponse.status(ex.getStatusCode())
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .bodyValue(ex.getReason()));
   }
 }
